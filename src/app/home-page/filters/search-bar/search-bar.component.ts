fac06ade4a12add1c844d71827e39e2d34e54be9
kh123 <kelvinahuynh@gmail.com>
2020-09-16 00:52:43 -0400

    viet character selection for input now working

--- src/app/home-page/filters/search-bar/search-bar.component.ts
+++ src/app/home-page/filters/search-bar/search-bar.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FiltersService } from '../filters.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent implements OnInit, OnDestroy {
   value = "";
+  sub: Subscription;
 
   constructor(public filterService: FiltersService) { }
 
 
   ngOnInit(): void {
+    this.sub = this.filterService.getFilterUserInputListener().subscribe(text => {
+      console.log('SUPE: ' + text);
+      if (this.value !== 'undefined') {
+        this.value = text;
+      }
+    })
   }
 
   onUpdateInput(userTypedInput) {
@@ -25,5 +33,9 @@ export class SearchBarComponent implements OnInit {
   clearFunction() {
     this.value = '';
     this.onUpdateInput('');
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
--- src/app/home-page/filters/viet-characters/viet-characters.component.ts
+++ src/app/home-page/filters/viet-characters/viet-characters.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { FiltersService } from '../filters.service';
@@ -8,9 +8,32 @@ import { FiltersService } from '../filters.service';
   templateUrl: './viet-characters.component.html',
   styleUrls: ['./viet-characters.component.css']
 })
-export class VietCharactersComponent implements OnInit {
+export class VietCharactersComponent implements OnInit, OnDestroy {
   vietLangFilterSelected = false;
   sub: Subscription;
+  sub2: Subscription;
+  currentUserFilterText: string;
+
+  aFirstRow = ['á', 'à', 'ả', 'ã', 'ạ', 'ă'];
+  aSecondRow = ['ắ', 'ằ', 'ẵ', 'ẳ', 'ặ', 'â'];
+  aThirdRow = ['ấ', 'ầ', 'ẩ', 'ẫ', 'ậ'];
+  dFirstRow = ['đ'];
+  dSecondRow = ['₫'];
+  eFirstRow = ['é', 'è', 'ẻ', 'ẽ'];
+  eSecondRow = ['ẹ', 'ê', 'ế', 'ề'];
+  eThirdRow = ['ể', 'ễ', 'ệ'];
+  iFirstRow = ['í', 'ì'];
+  iSecondRow = ['ỉ', 'ĩ'];
+  iThirdRow = ['ị'];
+  oFirstRow = ['ó', 'ò', 'ò', 'õ', 'ọ', 'ô'];
+  oSecondRow = ['ố', 'ồ', 'ổ', 'ỗ', 'ộ', 'ơ'];
+  oThirdRow = ['ớ', 'ờ', 'ở', 'ỡ', 'ợ'];
+  uFirstRow = ['ú', 'ù', 'ủ', 'ũ'];
+  uSecondRow = ['ụ', 'ư', 'ứ', 'ừ'];
+  uThirdRow = ['ử', 'ữ', 'ự'];
+  yFirstRow = ['ý', 'ỳ'];
+  ySecondRow = ['ỷ', 'ỹ'];
+  yThirdRow = ['ỵ'];
 
   constructor(public filterService: FiltersService) { }
 
@@ -24,10 +47,29 @@ export class VietCharactersComponent implements OnInit {
         this.vietLangFilterSelected = false;
       }
     });
+
+    this.sub2 = this.filterService.getFilterUserInputListener().subscribe(text => {
+      console.log('current user: ' + text);
+      this.currentUserFilterText = text;
+    })
+  }
+
+  insertIntoInput(letter) {
+    console.log('LETTER: ' + letter);
+    // update subject with previous subject value + selected vietnamese character
+    // if current subject value is undefined, update subject with just the character
+    if (this.currentUserFilterText) {
+      this.filterService.userTextFilterUpdated.next(this.currentUserFilterText + letter);
+    } else {
+      this.filterService.userTextFilterUpdated.next(letter);
+    }
+
+    this.filterService.changeFilterTextEvent();
   }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+    this.sub2.unsubscribe();
   }
 
 }
