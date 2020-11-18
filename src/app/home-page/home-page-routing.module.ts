75c13ad7ebf39734259872de3d5b7284800c5dd6
Kelvin Huynh <kelvinahuynh@gmail.com>
2020-11-18 02:39:19 -0500

    routing changes to implement new components

--- src/app/home-page/home-page-routing.module.ts
+++ src/app/home-page/home-page-routing.module.ts
@@ -4,18 +4,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page.component';
 import { PostsCreateComponent } from '../posts/posts-create/posts-create.component';
 import { ManageAccountComponent } from '../manage-account/manage-account.component';
+import { RenameCategoryComponent } from '../manage-account/rename-category/rename-category.component';
+import { DeleteCategoryComponent } from '../manage-account/delete-category/delete-category.component';
+import { ChangeEmailComponent } from '../manage-account/change-email/change-email.component';
+import { ChangePasswordComponent } from '../manage-account/change-password/change-password.component';
 
 const routes: Routes =[
   { path: 'home', component: HomePageComponent },
   { path: 'add-content', component: PostsCreateComponent },
   { path: 'edit-content/:postId', component: PostsCreateComponent },
-  { path: 'manage-account', component: ManageAccountComponent }
+  { path: 'manage-account', component: ManageAccountComponent },
+  { path: 'manage-account/rename-category', component: RenameCategoryComponent },
+  { path: 'manage-account/delete-category', component: DeleteCategoryComponent },
+  { path: 'manage-account/change-email', component: ChangeEmailComponent },
+  { path: 'manage-account/change-password', component: ChangePasswordComponent }
 ]
 
 @NgModule({
   imports: [
     RouterModule.forChild(routes)
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
 })
 export class HomePageRoutingModule {}
--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { HomePageComponent } from './home-page.component';
 import { ManageAccountComponent } from '../manage-account/manage-account.component';
+import { RenameCategoryComponent } from '../manage-account/rename-category/rename-category.component';
 import { CategoriesComponent } from './filters/categories/categories.component';
 import { RadioFilterBtnsComponent } from './filters/radio-display-btns/radio-display-btns.component';
 import { SearchBarComponent } from './filters/search-bar/search-bar.component';
@@ -13,11 +14,20 @@ import { HeaderModule } from '../header/header.module';
 import { PostsModule } from '../posts/posts.module';
 import { AngularMaterialModule } from '../angular-material.module';
 import { HomePageRoutingModule } from './home-page-routing.module';
+import { SettingTemplateComponent } from '../manage-account/setting-template/setting-template.component';
+import { ChangeEmailComponent } from '../manage-account/change-email/change-email.component';
+import { ChangePasswordComponent } from '../manage-account/change-password/change-password.component';
+import { DeleteCategoryComponent } from '../manage-account/delete-category/delete-category.component';
 
 @NgModule({
   declarations: [
     HomePageComponent,
     ManageAccountComponent,
+    RenameCategoryComponent,
+    DeleteCategoryComponent,
+    ChangePasswordComponent,
+    ChangeEmailComponent,
+    SettingTemplateComponent,
     CategoriesComponent,
     RadioFilterBtnsComponent,
     SearchBarComponent,
