ace547784cd3f34511d7a8a631b5439b41f342c7
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-05-18 19:31:31 -0400

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://viet4me.herokuapp.com/landing-page',
+    apiUrl: 'viet4me.herokuapp.com/',
     GOOGLE_APPLICATION_CREDENTIALS: 'AIzaSyAV62NG_GxeEm2E4K1oPPKI_yWXT1r3UW8',
     DB_URL: "mongodb+srv://kelvin:JAL8sHG52ld7qaKD@viet4me.vqq1u.mongodb.net/viet4me?retryWrites=true&w=majority"
   };
