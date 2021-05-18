09e236b74e1890ccf6780811782da29c0c217451
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-05-18 02:48:34 -0400

    ignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://viet4me.herokuapp.com',
+    GOOGLE_APPLICATION_CREDENTIALS: 'AIzaSyAV62NG_GxeEm2E4K1oPPKI_yWXT1r3UW8'
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/api',
+    GOOGLE_APPLICATION_CREDENTIALS: 'AIzaSyAV62NG_GxeEm2E4K1oPPKI_yWXT1r3UW8'
+  };
